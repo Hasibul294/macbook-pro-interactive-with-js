201cb802bdc62dddd326f81245bdf6b94eca583c
Hasibul294 <hasibulhasan294@gmail.com>
2021-08-21 15:19:05 +0600

    comment line added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Total price function 
 function getTotal(){
     let totalCost = 1299;
 
@@ -14,6 +15,7 @@ function getTotal(){
 
     return totalCostAmount;
 }
+// total cost after adding promo code function 
 function getTotalWithPromoCode(promoCode){
     const promoCodeInputField = document.getElementById('promo-code-input');
     if(promoCodeInputField.value == promoCode){
@@ -31,51 +33,52 @@ function getTotalWithPromoCode(promoCode){
     promoCodeInputField.value = '';
 
 }
+// event handler for 8gb memory button 
 document.getElementById('8gb-memory-button').addEventListener('click', function(){
     const extraMemory = document.getElementById('memory-cost');
     extraMemory.innerText = 0;
     const totalPrice = getTotal();
 });
-
+// event handler for 16gb memory button 
 document.getElementById('16gb-memory-button').addEventListener('click', function(){
     const extraMemory = document.getElementById('memory-cost');
     extraMemory.innerText = 180;
     const totalPrice = getTotal();
 });
-
+// event handler for 256gb ssd button 
 document.getElementById('256gb-ssd-button').addEventListener('click', function(){
     const extraStorage = document.getElementById('storage-cost');
     extraStorage.innerText = 0;
     const totalPrice = getTotal();
 
 });
-
+// event handler for 512gb ssd button 
 document.getElementById('512gb-ssd-button').addEventListener('click', function(){
     const extraStorage = document.getElementById('storage-cost');
     extraStorage.innerText = 100;
     const totalPrice = getTotal();
 
 });
-
+// event handler for 1tb ssd button 
 document.getElementById('1tb-ssd-button').addEventListener('click', function(){
     const extraStorage = document.getElementById('storage-cost');
     extraStorage.innerText = 180;
     const totalPrice = getTotal();
 
 });
-
+// event handler for free delivery button 
 document.getElementById('free-delivery').addEventListener('click', function(){
     const deliveryCharge = document.getElementById('delivery-cost');
     deliveryCharge.innerText = 0;
     const totalPrice = getTotal();
 });
-
+// event handler for cost delivery button 
 document.getElementById('cost-delivery').addEventListener('click', function(){
     const deliveryCharge = document.getElementById('delivery-cost');
     deliveryCharge.innerText = 20;
     const totalPrice = getTotal();
 });
-
+// event handler for promo code button 
 document.getElementById('promo-code-button').addEventListener('click', function(){
     const promoCode = 'stevekaku';
     getTotalWithPromoCode(promoCode);
